08dd361ac69e8e4988bf073f14e8336b36206c97
Luis Marqueta <lmarqueta@gmail.com>
2017-01-27 12:33:22 +0100

    testing

--- apitest.py
+++ apitest.py
@@ -1,8 +1,16 @@
 import telepot as t
 from pprint import pprint
+import sys
 import config
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        msg = sys.argv[1]
+    else:
+        err = "Usage: {} <message>\n".format(sys.argv[0])
+        sys.stderr.write(err)
+        sys.exit(1)
+
     bot = t.Bot(config.token)
     print bot.getMe()
 
@@ -11,5 +19,4 @@ if __name__ == "__main__":
     pprint(response)
 
     # Send a message
-    msg = "Test message"
     bot.sendMessage(config.id, msg)
